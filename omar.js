2a2ec523956cdbd3a13ad1e37e7af9d0fab7ee7b
Mark <markwilkins86@gmail.com>
2022-05-10 16:44:20 -0400

    Updates - 5/10

--- omar.js
+++ omar.js
@@ -1,52 +1,108 @@
 let dataFetch = () => {
-    fetch('http://hp-api.herokuapp.com/api/characters').then(resp => resp.json()).then(data => someFunction(data))
+    fetch('http://hp-api.herokuapp.com/api/characters').then(resp => resp.json()).then(data => someFunction(data)).catch(error => console.log(error))
 }
 
 dataFetch();
 
 let searchBar = document.getElementById('search-bar');
 let charactersReturnedList = document.getElementById('search-result-list');
-let nameInput = document.getElementById('name-input')
-let houseInput = document.getElementById('house-input')
-let profileImage = document.getElementById('character-image')
+let nameInput = document.getElementById('name-input');
+let houseInput = document.getElementById('house-input');
+let profileImage = document.getElementById('character-image');
 
+let learnMoreButton = document.getElementById('learn-more-button')
+
+let speciesInput = document.getElementById('species-input');
+let speciesDiv = document.querySelector('.species-div');
+
+let birthInput = document.getElementById('birth-input');
+let birthDiv = document.querySelector('.birth-div');
+
+let ancestryInput = document.getElementById('ancestry-input');
+let ancestryDiv = document.querySelector('.ancestry-div');
+
+let addClassButton = document.getElementById('add-to-class-button');
+let classSelectedList = document.getElementById('class-selected-list')
 
 let someFunction = (data) => {
     searchBar.addEventListener("keydown", (e) => {
         if (e.key == "Enter" && searchBar.value != "") {
             let searchQuery = searchBar.value;
-            // console.log(searchQuery)
+            searchBar.value = '';
             let searchResult = data.filter(character =>{
-                    // console.log(character.name.toLowerCase().includes(searchQuery));
                     if (character.name.toLowerCase().includes(searchQuery.toLowerCase())){
-                        // console.log(character);
                             let listOfCharacters = document.createElement('li');
                             listOfCharacters;
                             listOfCharacters.textContent = character.name;
 
-                            // Mark - NEWLY ADDED at 10:30 AM - Clear List Button
-                            let button = document.getElementById('clearList')
+                            let button = document.getElementById('clearList');
                             button.addEventListener('click', (e) => {
-                                    document.getElementById('search-result-list').innerHTML='';
-                                })
-                            
-                                
+                                document.getElementById('search-result-list').innerHTML='';
+                            })
 
                             listOfCharacters.addEventListener('mouseover' , onHoverFunction);
                             listOfCharacters.addEventListener('mouseout' , offHoverFunction);
 
+                            //function for when user selects character from list, to iterate through data to find that exact character and add it to profile div
                             listOfCharacters.addEventListener('click' , function(event){
                                 data.find(character =>{
-                                    // console.log(character.name === event.target.textContent)
                                     if (character.name === event.target.textContent){
-                                        // console.log(character);
                                         nameInput.textContent = character.name;
-                                        houseInput.textContent = character.house;
+
+                                        //makes it so that if the character doesnt have a house the value is set to the default N/A
+                                        if (character.house !== ''){
+                                            houseInput.textContent = character.house;
+                                        }
+                                        if (character.house === ''){
+                                            houseInput.textContent = 'N/A'; 
+                                        }
+
                                         if (character.image !== ""){
                                             profileImage.src = character.image;
                                         } if (character.image === ""){
                                             profileImage.src = 'https://cdn-icons-png.flaticon.com/512/1600/1600953.png'; 
                                         }
+
+                                        if (character.species !== ''){
+                                            speciesInput.textContent = character.species;
+                                        } if (character.species === ''){
+                                            speciesInput.textContent = 'N/A';
+                                        }
+
+                                        if (character.dateOfBirth !== ''){
+                                            birthInput.textContent = character.dateOfBirth;
+                                        } if (character.dateOfBirth === ''){
+                                            birthInput.textContent = 'N/A';
+                                        }
+
+                                        if (character.ancestry !== ''){
+                                            ancestryInput.textContent = character.ancestry;
+                                        } if (character.ancestry === ''){
+                                            ancestryInput.textContent = 'N/A';
+                                        }
+
+                                        //function to add the character in the profile to the class list div
+                                        addClassButton.addEventListener('click' , ()=>{
+                                            let listOfSelectedCharacters = document.createElement('li');
+                                            listOfSelectedCharacters;
+                                            // console.log(nameInput.textContent);
+                                            listOfSelectedCharacters.textContent = nameInput.textContent;
+
+                                            //next 4 lines of code are to get around having to set the nameInput.textContent to blank, and having the name in the
+                                            //profile be blank so make everything blank? lol
+                                            classSelectedList.append(listOfSelectedCharacters);
+                                            nameInput.textContent = '';
+                                            houseInput.textContent = '';
+                                            profileImage.src = 'https://cdn-icons-png.flaticon.com/512/1600/1600953.png';
+                                            speciesInput.textContent = ''; 
+                                            birthInput.textContent = '';
+                                            ancestryInput.textContent = '';
+
+
+                                            if (listOfSelectedCharacters.textContent === ''){
+                                                listOfSelectedCharacters.remove();
+                                            }
+                                        })
                                     }
                                 })
                             })
@@ -64,3 +120,14 @@ let onHoverFunction = event =>{
 let offHoverFunction = event => {
     event.target.style.color = "black";
 }
+
+function darkMode() {
+    let mode = document.body
+    mode.classList.toggle('dark-mode')
+    
+}
+
+function myFunction() {
+    let learnMoreSection = document.getElementById('learn-more-sub-section');
+    learnMoreSection.classList.toggle("visible-learn-more");
+  }
